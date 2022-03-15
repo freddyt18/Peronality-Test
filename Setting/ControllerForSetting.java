93e919050bbb864ae9de51e484432be6a5bb08c0
whatanakKean <keanwhatanak@gmail.com>
2022-03-15 16:20:07 +0700

    helllo

--- Setting/ControllerForSetting.java
+++ Setting/ControllerForSetting.java
@@ -2,15 +2,21 @@ package Setting;
 
 import java.io.IOException;
 
+import App.App;
+import Data.Data_Handling.DataHandling;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class ControllerForSetting {
+    App app = new App();
+    DataHandling dataHandling = new DataHandling();
+
     //new cshanges
     @FXML
     private BorderPane SettingBorderPane;
@@ -53,5 +59,27 @@ public class ControllerForSetting {
     void goToTerm(ActionEvent event) throws IOException{
         SettingBorderPane.setCenter(FXMLLoader.load(getClass().getResource("Term.fxml")));
     }
+//////////////////////Change Profile
+    @FXML
+    private Button BtnChangeProfile;
 
+    @FXML
+    private TextField NewBio;
+
+    @FXML
+    private TextField NewEmail;
+
+    @FXML
+    private TextField NewName;
+
+    @FXML
+    private TextField NewSex;
+
+    @FXML
+    private TextField NewUsername;
+    @FXML
+    void btnSubmitChangeProfile(ActionEvent event) {
+        DataHandling.users.get(App.CURRENT_USER_ID).setUsername(NewUsername.getText());
+        System.out.println(DataHandling.users.get(App.CURRENT_USER_ID).getUsername().toString());
+    }
 }
