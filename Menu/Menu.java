626180efb36ae75ef1816e387d749b7e0cf3af90
freddyt18 <keopitou.doung@student.cadt.edu.kh>
2022-03-18 22:04:59 +0700

    New

---
+++ Menu/Menu.java
@@ -0,0 +1,35 @@
+package Menu;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Menu {
+    FXMLLoader loader = new FXMLLoader();
+    public static Stage current = new Stage();
+    static Parent root;
+    static Scene scene;
+    private boolean firstTime = true;
+
+
+    public void showMenu() throws IOException { 
+        if(firstTime){
+            loader.setLocation(Menu.class.getResource("menu.fxml"));
+            root = loader.load();
+            scene = new Scene(root, 1200, 657.53);
+            current.setScene(scene);
+            firstTime = false;
+        }
+        current.setTitle("Test");
+        current.getIcons().add(new Image("https://cdn.iconscout.com/icon/premium/png-64-thumb/personality-disorder-2708518-2248491.png"));
+        current.show();
+    }
+
+    public void closeMenu() throws IOException {
+        current.close();
+    }
+}
