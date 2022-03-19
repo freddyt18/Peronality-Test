491ce35bd4ee574411cc78cfc5554e66295ce8b4
freddyt18 <keopitou.doung@student.cadt.edu.kh>
2022-03-19 17:00:49 +0700

    Test

--- Test/ControllerForTest.java
+++ Test/ControllerForTest.java
@@ -1,12 +1,15 @@
 package Test;
 
 import java.beans.EventHandler;
+import java.security.spec.ECField;
 
+import Profile.profile;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.effect.Effect;
 
 public class ControllerForTest {
     
@@ -50,396 +53,441 @@ public class ControllerForTest {
     private RadioButton q101, q102, q103, q104, q105, q106, q107;
 
     public void submitButtonPressed(){
-        getQ1();
-        getQ2();
-        getQ3();
-        getQ4();
-        getQ5();
-        getQ6();
-        getQ7();
-        getQ8();
-        getQ9();
-        getQ10();
-        System.out.println("extro: "+extroversion);
+        openness = oForQ5 + oForQ6 + oForQ7 + oForQ9 + oForQ10;
+        conscientiousness = cForQ3 + cForQ4 + cForQ5 + cForQ6 + cForQ7 + cForQ8;
+        extroversion = eForQ1 + eForQ2 + eForQ3 + eForQ4 + eForQ5 + eForQ6;
+        agreeableness = aForQ2 + aForQ3 + aForQ4 + aForQ5 + aForQ6 + aForQ7;
+        neroticism = nForQ4 + nForQ5 + nForQ6 + nForQ7 + nForQ8 + nForQ9;
+        System.out.println("Openness: " + openness + "\nConscienciousness: " + conscientiousness + "\nExtroversion: " + extroversion + "\nAgreeableness: " + agreeableness + "\nNeroticism: " + neroticism);
     }
 
+    int eForQ1 = 0;
     public void getQ1(){
+        eForQ1 = 0;
+        int temp = 0;
         if(q11.isSelected()){
-            extroversion += 1;
+            temp += 1;
         }
         else if(q12.isSelected()){
-            extroversion += 2;
+            temp += 2;
         }
         else if(q13.isSelected()){
-            extroversion += 3;
+            temp += 3;
         }
         else if(q14.isSelected()){
-            extroversion += 4;
+            temp += 4;
         }
         else if(q15.isSelected()){
-            extroversion += 5;
+            temp += 5;
         }
         else if(q16.isSelected()){
-            extroversion += 6;
+            temp += 6;
         }
         else if(q17.isSelected()){
-            extroversion += 7;
+            temp += 7;
         }
+        eForQ1 = temp;
     }
 
+    int eForQ2 = 0, aForQ2 = 0;
     public void getQ2(){
+        eForQ2 = 0;
+        aForQ2 = 0;
+
         if(q21.isSelected()){
-            extroversion += 1;
-            agreeableness += 1;
+            eForQ2 += 1;
+            aForQ2 += 1;
         }
         else if(q22.isSelected()){
-            extroversion += 2;
-            agreeableness+= 2;
+            eForQ2 += 2;
+            aForQ2+= 2;
         }
         else if(q23.isSelected()){
-            extroversion += 3;
-            agreeableness += 3;
+            eForQ2 += 3;
+            aForQ2 += 3;
         }
         else if(q24.isSelected()){
-            extroversion += 4;
-            agreeableness += 4;
+            eForQ2 += 4;
+            aForQ2 += 4;
         }
         else if(q25.isSelected()){
-            extroversion += 5;
-            agreeableness += 5;
+            eForQ2 += 5;
+            aForQ2 += 5;
         }
         else if(q26.isSelected()){
-            extroversion += 6;
-            agreeableness += 6;
+            eForQ2 += 6;
+            aForQ2 += 6;
         }
         else if(q27.isSelected()){
-            extroversion += 7;
-            agreeableness += 7;
+            eForQ2 += 7;
+            aForQ2 += 7;
         }
     }
 
+    int eForQ3 = 0, aForQ3 = 0, cForQ3 = 0;
     public void getQ3(){
+        eForQ3 = 0;
+        aForQ3 = 0;
+        cForQ3 = 0;
+
         if(q31.isSelected()){
-            extroversion += 1;
-            agreeableness += 1;
-            conscientiousness += 1;
+            eForQ3 += 1;
+            aForQ3 += 1;
+            cForQ3 += 1;
         }
         else if(q32.isSelected()){
-            extroversion += 2;
-            agreeableness+= 2;
-            conscientiousness += 2;
+            eForQ3 += 2;
+            aForQ3+= 2;
+            cForQ3 += 2;
         }
         else if(q33.isSelected()){
-            extroversion += 3;
-            agreeableness += 3;
-            conscientiousness += 3;
+            eForQ3 += 3;
+            aForQ3 += 3;
+            cForQ3 += 3;
         }
         else if(q34.isSelected()){
-            extroversion += 4;
-            agreeableness += 4;
-            conscientiousness += 4;
+            eForQ3 += 4;
+            aForQ3 += 4;
+            cForQ3 += 4;
         }
         else if(q35.isSelected()){
-            extroversion += 5;
-            agreeableness += 5;
-            conscientiousness += 5;
+            eForQ3 += 5;
+            aForQ3 += 5;
+            cForQ3 += 5;
         }
         else if(q36.isSelected()){
-            extroversion += 6;
-            agreeableness += 6;
-            conscientiousness += 6;
+            eForQ3 += 6;
+            aForQ3 += 6;
+            cForQ3 += 6;
         }
         else if(q37.isSelected()){
-            extroversion += 7;
-            agreeableness += 7;
-            conscientiousness += 7;
+            eForQ3 += 7;
+            aForQ3 += 7;
+            cForQ3 += 7;
         }
     }
 
+    int eForQ4 = 0, aForQ4 = 0, cForQ4 = 0, nForQ4 = 0;
     public void getQ4(){
+        eForQ4 = 0;
+        aForQ4 = 0; 
+        cForQ4 = 0;
+        nForQ4 = 0;
+
         if(q41.isSelected()){
-            extroversion += 1;
-            agreeableness += 1;
-            conscientiousness += 1;
-            neroticism += 1;
+            eForQ4 += 1;
+            aForQ4 += 1;
+            cForQ4 += 1;
+            nForQ4 += 1;
         }
         else if(q42.isSelected()){
-            extroversion += 2;
-            agreeableness+= 2;
-            conscientiousness += 2;
-             neroticism += 2;
+            eForQ4 += 2;
+            aForQ4+= 2;
+            cForQ4 += 2;
+            nForQ4 += 2;
         }
         else if(q43.isSelected()){
-            extroversion += 3;
-            agreeableness += 3;
-            conscientiousness += 3;
-            neroticism += 3;
+            eForQ4 += 3;
+            aForQ4 += 3;
+            cForQ4 += 3;
+            nForQ4 += 3;
         }
         else if(q44.isSelected()){
-            extroversion += 4;
-            agreeableness += 4;
-            conscientiousness += 4;
-            neroticism += 4;
+            eForQ4 += 4;
+            aForQ4 += 4;
+            cForQ4 += 4;
+            nForQ4 += 4;
         }
         else if(q45.isSelected()){
-            extroversion += 5;
-            agreeableness += 5;
-            conscientiousness += 5;
-            neroticism += 5;
+            eForQ4 += 5;
+            aForQ4 += 5;
+            cForQ4 += 5;
+            nForQ4 += 5;
         }
         else if(q46.isSelected()){
-            extroversion += 6;
-            agreeableness += 6;
-            conscientiousness += 6;
-            neroticism += 6;
+            eForQ4 += 6;
+            aForQ4 += 6;
+            cForQ4 += 6;
+            nForQ4 += 6;
         }
         else if(q47.isSelected()){
-            extroversion += 7;
-            agreeableness += 7;
-            conscientiousness += 7;
-            neroticism += 7;
+            eForQ4 += 7;
+            aForQ4 += 7;
+            cForQ4 += 7;
+            nForQ4 += 7;
         }
     }
 
+    int eForQ5 = 0, aForQ5 = 0, cForQ5 = 0, nForQ5 = 0, oForQ5 = 0;
     public void getQ5(){
+        eForQ5 = 0;
+        aForQ5 = 0; 
+        cForQ5 = 0; 
+        nForQ5 = 0; 
+        oForQ5 = 0;
+
         if(q51.isSelected()){
-            extroversion += 1;
-            agreeableness += 1;
-            conscientiousness += 1;
-            neroticism += 1;
-            openness += 1;
+            eForQ5 += 1;
+            aForQ5 += 1;
+            cForQ5 += 1;
+            nForQ5 += 1;
+            oForQ5 += 1;
         }
         else if(q52.isSelected()){
-            extroversion += 2;
-            agreeableness+= 2;
-            conscientiousness += 2;
-             neroticism += 2;
-             openness += 2;
+            eForQ5 += 2;
+            aForQ5+= 2;
+            cForQ5 += 2;
+            nForQ5 += 2;
+            oForQ5 += 2;
         }
         else if(q53.isSelected()){
-            extroversion += 3;
-            agreeableness += 3;
-            conscientiousness += 3;
-            neroticism += 3;
-            openness += 3;
+            eForQ5 += 3;
+            aForQ5 += 3;
+            cForQ5 += 3;
+            nForQ5 += 3;
+            oForQ5 += 3;
         }
         else if(q54.isSelected()){
-            extroversion += 4;
-            agreeableness += 4;
-            conscientiousness += 4;
-            neroticism += 4;
-            openness += 4;
+            eForQ5 += 4;
+            aForQ5 += 4;
+            cForQ5 += 4;
+            nForQ5 += 4;
+            oForQ5 += 4;
         }
         else if(q55.isSelected()){
-            extroversion += 5;
-            agreeableness += 5;
-            conscientiousness += 5;
-            neroticism += 5;
-            openness += 5;
+            eForQ5 += 5;
+            aForQ5 += 5;
+            cForQ5 += 5;
+            nForQ5 += 5;
+            oForQ5 += 5;
         }
         else if(q56.isSelected()){
-            extroversion += 6;
-            agreeableness += 6;
-            conscientiousness += 6;
-            neroticism += 6;
-            openness += 6;
+            eForQ5 += 6;
+            aForQ5 += 6;
+            cForQ5 += 6;
+            nForQ5 += 6;
+            oForQ5 += 6;
         }
         else if(q57.isSelected()){
-            extroversion += 7;
-            agreeableness += 7;
-            conscientiousness += 7;
-            neroticism += 7;
-            openness += 7;
+            eForQ5 += 7;
+            aForQ5 += 7;
+            cForQ5 += 7;
+            nForQ5 += 7;
+            oForQ5 += 7;
         }
     }
 
+    int eForQ6 = 0, aForQ6 = 0, cForQ6 = 0, nForQ6 = 0, oForQ6 = 0;
     public void getQ6(){
+        eForQ6 = 0;
+        aForQ6 = 0;
+        cForQ6 = 0;
+        nForQ6 = 0;
+        oForQ6 = 0;
+
         if(q61.isSelected()){
-            extroversion += 1;
-            agreeableness += 1;
-            conscientiousness += 1;
-            neroticism += 1;
-            openness += 1;
+            eForQ6 += 1;
+            aForQ6 += 1;
+            cForQ6 += 1;
+            nForQ6 += 1;
+            oForQ6 += 1;
         }
         else if(q62.isSelected()){
-            extroversion += 2;
-            agreeableness+= 2;
-            conscientiousness += 2;
-             neroticism += 2;
-             openness += 2;
+            eForQ6 += 2;
+            aForQ6+= 2;
+            cForQ6 += 2;
+            nForQ6 += 2;
+            oForQ6 += 2;
         }
         else if(q63.isSelected()){
-            extroversion += 3;
-            agreeableness += 3;
-            conscientiousness += 3;
-            neroticism += 3;
-            openness += 3;
+            eForQ6 += 3;
+            aForQ6 += 3;
+            cForQ6 += 3;
+            nForQ6 += 3;
+            oForQ6 += 3;
         }
         else if(q64.isSelected()){
-            extroversion += 4;
-            agreeableness += 4;
-            conscientiousness += 4;
-            neroticism += 4;
-            openness += 4;
+            eForQ6 += 4;
+            aForQ6 += 4;
+            cForQ6 += 4;
+            nForQ6 += 4;
+            oForQ6 += 4;
         }
         else if(q65.isSelected()){
-            extroversion += 5;
-            agreeableness += 5;
-            conscientiousness += 5;
-            neroticism += 5;
-            openness += 5;
+            eForQ6 += 5;
+            aForQ6 += 5;
+            cForQ6 += 5;
+            nForQ6 += 5;
+            oForQ6 += 5;
         }
         else if(q66.isSelected()){
-            extroversion += 6;
-            agreeableness += 6;
-            conscientiousness += 6;
-            neroticism += 6;
-            openness += 6;
+            eForQ6 += 6;
+            aForQ6 += 6;
+            cForQ6 += 6;
+            nForQ6 += 6;
+            oForQ6 += 6;
         }
         else if(q67.isSelected()){
-            extroversion += 7;
-            agreeableness += 7;
-            conscientiousness += 7;
-            neroticism += 7;
-            openness += 7;
+            eForQ6 += 7;
+            aForQ6 += 7;
+            cForQ6 += 7;
+            nForQ6 += 7;
+            oForQ6 += 7;
         }
     }
 
+    int aForQ7 = 0, cForQ7 = 0, nForQ7 = 0, oForQ7 = 0;
     public void getQ7(){
+        aForQ7 = 0;
+        cForQ7 = 0;
+        nForQ7 = 0;
+        oForQ7 = 0;
+
         if(q71.isSelected()){
-            agreeableness += 1;
-            conscientiousness += 1;
+            aForQ7 += 1;
+            cForQ7 += 1;
             neroticism += 1;
-            openness += 1;
+            oForQ7 += 1;
         }
         else if(q72.isSelected()){
-            agreeableness+= 2;
-            conscientiousness += 2;
-             neroticism += 2;
-             openness += 2;
+            aForQ7+= 2;
+            cForQ7 += 2;
+            nForQ7 += 2;
+            oForQ7 += 2;
         }
         else if(q73.isSelected()){
-            agreeableness += 3;
-            conscientiousness += 3;
-            neroticism += 3;
-            openness += 3;
+            aForQ7 += 3;
+            cForQ7 += 3;
+            nForQ7 += 3;
+            oForQ7 += 3;
         }
         else if(q74.isSelected()){
-            agreeableness += 4;
-            conscientiousness += 4;
-            neroticism += 4;
-            openness += 4;
+            aForQ7 += 4;
+            cForQ7 += 4;
+            nForQ7 += 4;
+            oForQ7 += 4;
         }
         else if(q75.isSelected()){
-            agreeableness += 5;
-            conscientiousness += 5;
-            neroticism += 5;
-            openness += 5;
+            aForQ7 += 5;
+            cForQ7 += 5;
+            nForQ7 += 5;
+            oForQ7 += 5;
         }
         else if(q76.isSelected()){
-            agreeableness += 6;
-            conscientiousness += 6;
-            neroticism += 6;
-            openness += 6;
+            aForQ7 += 6;
+            cForQ7 += 6;
+            nForQ7 += 6;
+            oForQ7 += 6;
         }
         else if(q77.isSelected()){
-            agreeableness += 7;
-            conscientiousness += 7;
-            neroticism += 7;
-            openness += 7;
+            aForQ7 += 7;
+            cForQ7 += 7;
+            nForQ7 += 7;
+            oForQ7 += 7;
         }
     }
 
+    int cForQ8 = 0, nForQ8 = 0, oForQ8 = 0;
     public void getQ8(){
+        cForQ8 = 0;
+        nForQ8 = 0;
+        oForQ8 = 0;
+
         if(q81.isSelected()){
-            conscientiousness += 1;
-            neroticism += 1;
-            openness += 1;
+            cForQ8 += 1;
+            nForQ8 += 1;
+            oForQ8 += 1;
         }
         else if(q82.isSelected()){
-            conscientiousness += 2;
-             neroticism += 2;
-             openness += 2;
+            cForQ8 += 2;
+            nForQ8 += 2;
+            oForQ8 += 2;
         }
         else if(q83.isSelected()){
-            conscientiousness += 3;
-            neroticism += 3;
-            openness += 3;
+            cForQ8 += 3;
+            nForQ8 += 3;
+            oForQ8 += 3;
         }
         else if(q84.isSelected()){
-            conscientiousness += 4;
-            neroticism += 4;
-            openness += 4;
+            cForQ8 += 4;
+            nForQ8 += 4;
+            oForQ8 += 4;
         }
         else if(q85.isSelected()){
-            conscientiousness += 5;
-            neroticism += 5;
-            openness += 5;
+            cForQ8 += 5;
+            nForQ8 += 5;
+            oForQ8 += 5;
         }
         else if(q86.isSelected()){
-            conscientiousness += 6;
-            neroticism += 6;
-            openness += 6;
+            cForQ8 += 6;
+            nForQ8 += 6;
+            oForQ8 += 6;
         }
         else if(q87.isSelected()){
-            conscientiousness += 7;
-            neroticism += 7;
-            openness += 7;
+            cForQ8 += 7;
+            nForQ8 += 7;
+            oForQ8 += 7;
         }
     }
 
+    int nForQ9 = 0, oForQ9 = 0;
     public void getQ9(){
+        nForQ9 = 0;
+        oForQ9 = 0;
+
         if(q91.isSelected()){
-            neroticism += 1;
-            openness += 1;
+            nForQ9 += 1;
+            oForQ9 += 1;
         }
         else if(q92.isSelected()){
-             neroticism += 2;
-             openness += 2;
+            nForQ9 += 2;
+            oForQ9 += 2;
         }
         else if(q93.isSelected()){
-            neroticism += 3;
-            openness += 3;
+            nForQ9 += 3;
+            oForQ9 += 3;
         }
         else if(q94.isSelected()){
-            neroticism += 4;
-            openness += 4;
+            nForQ9 += 4;
+            oForQ9 += 4;
         }
         else if(q95.isSelected()){
-            neroticism += 5;
-            openness += 5;
+            nForQ9 += 5;
+            oForQ9 += 5;
         }
         else if(q96.isSelected()){
-            neroticism += 6;
-            openness += 6;
+            nForQ9 += 6;
+            oForQ9 += 6;
         }
         else if(q97.isSelected()){
-            neroticism += 7;
-            openness += 7;
+            nForQ9 += 7;
+            oForQ9 += 7;
         }
     }
 
+    int oForQ10 = 0;
     public void getQ10(){
+        oForQ10 = 0;
         if(q101.isSelected()){
-            openness += 1;
+            oForQ10 += 1;
         }
         else if(q102.isSelected()){
-             openness += 2;
+            oForQ10 += 2;
         }
         else if(q103.isSelected()){
-            openness += 3;
+            oForQ10 += 3;
         }
         else if(q104.isSelected()){
-            openness += 4;
+            oForQ10 += 4;
         }
         else if(q105.isSelected()){
-            openness += 5;
+            oForQ10 += 5;
         }
         else if(q106.isSelected()){
-            openness += 6;
+            oForQ10 += 6;
         }
         else if(q107.isSelected()){
-            openness += 7;
+            oForQ10 += 7;
         }
     }
 }
