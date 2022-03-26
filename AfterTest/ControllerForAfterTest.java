aa6a540bdab768639287facc07bc1b1918c4825e
freddyt18 <keopitou.doung@student.cadt.edu.kh>
2022-03-26 13:50:36 +0700

    match

--- AfterTest/ControllerForAfterTest.java
+++ AfterTest/ControllerForAfterTest.java
@@ -24,13 +24,46 @@ public class ControllerForAfterTest {
     private Button back;
 
     @FXML
-    private void afterPressed(){
+    private Label ptype;
+
+    @FXML
+    private Label u1, u2, u3, u4;
+
+    @FXML
+    private void afterPressed() throws IOException {
         details.setText(App.rg.personality_type_result.get(Integer.parseInt(DataHandling.users.get(App.CURRENT_USER_ID).getPersonality().replace("#", ""))));
+        setEmailForMatch();
+        ptype.setText(DataHandling.users.get(App.CURRENT_USER_ID).getPersonality());
     }
 
     @FXML
     private void afterBackPressed() throws IOException {
         af.closeAfterTest();
         m.showMenu();
+    }
+
+    private void setEmailForMatch() throws IOException {
+        int u = 0;
+        String temp = DataHandling.users.get(App.CURRENT_USER_ID).getPersonality();
+        for(int i = 0; i < DataHandling.users.size(); i++){
+            if(i==App.CURRENT_USER_ID){
+                continue;
+            }
+            if(DataHandling.users.get(i).getPersonality().equals(temp)){
+                if(u==0){
+                    u++;
+                    u1.setText(DataHandling.users.get(i).getEmail());
+                } else if(u==1){
+                    u++;
+                    u2.setText(DataHandling.users.get(i).getEmail());
+                } else if(u==2){
+                    u++;
+                    u3.setText(DataHandling.users.get(i).getEmail());
+                } else {
+                    u++;
+                    u4.setText(DataHandling.users.get(i).getEmail());
+                }
+            }
+        }
     }
 }
